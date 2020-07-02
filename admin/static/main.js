3ae9782a20e10e040282e11fbd5a087083d0f3dc
The5teve <61376282+The5teve@users.noreply.github.com>
2020-07-03 00:35:30 +0300

    Add files via upload

--- admin/static/main.js
+++ admin/static/main.js
@@ -1,41 +1,99 @@
+function editRecord(selectRecord){
 
-function renderRecordsSelect(selectRecords) {
-    let adm = document.getElementById('admArea1');
-    let typ = document.getElementById('typObj1');
-    let dist = document.getElementById('dist1');
-    let option;
-    let option1;
-    let option2;
+    let name = document.getElementById('name');
+    name.value= selectRecord.name;
+    let address = document.getElementById('address1');
+    address.value= selectRecord.address;
 
-    for (selectRecord of selectRecords) {
-    if (!(selectRecord.admArea in myset)){
+    let admArea = document.getElementById('admArea1');
+    admArea.value= selectRecord.admArea;
+
+    let district = document.getElementById('dist1');
+    district.value= selectRecord.district;
+
+    if (selectRecord.isNetObject==1){
+
+    let isNetObject = document.getElementById('isNetTrue');
+    isNetObject.checked= true;
+    } 
+    else {
+        
+        let isNetObject = document.getElementById('isNetFalse');
+        isNetObject.checked= true;
+    }
+    let operatingCompany = document.getElementById('OperatingCompany');
+    operatingCompany.value= selectRecord.operatingCompany;
+
+    let publicPhone = document.getElementById('publicPhone');
+    publicPhone.value= selectRecord.publicPhone;
+
+    let seatsCount = document.getElementById('SeatsCount');
+    seatsCount.value= selectRecord.seatsCount;
+
+    let typeObject = document.getElementById('typObj1');
+    typeObject.value= selectRecord.typeObject;
+    if (selectRecord.socialPrivileges==1){
+
+        let socialPrivileges = document.getElementById('socialPrivilegesTrue');
+        socialPrivileges.checked= true;
+        } 
+    else {
+            
+            let socialPrivileges = document.getElementById('socialPrivilegesFalse');
+            socialPrivileges.checked= true;
+        }
+/*
+    let name = document.getElementById('name');
+    name.value= selectRecord.name;
+    let name = document.getElementById('name');
+    name.value= selectRecord.name;
+    */
+}
+function AddOption(Set, idOfEl){
+temp=document.getElementById(idOfEl);
+let option;
+    for (selectRecord of Set) {
         option = document.createElement('option');
-        option.innerHTML = selectRecord.admArea;
-        option.value = selectRecord.admArea;
-        adm.append(option);
-        myset.add(selectRecord.admArea);
-    } 
-    if (!(selectRecord.typeObject in myset)){
-        option1 = document.createElement('option');
-        option1.innerHTML = selectRecord.typeObject;
-        option1.value = selectRecord.typeObject;
-        typ.append(option1);
-        myset.add(selectRecord.typeObject);
-    } 
-    if (!(selectRecord.district in myset)){
-        option2 = document.createElement('option');
-        option2.innerHTML = selectRecord.district;
-        option2.value = selectRecord.district;
-        dist.append(option2);
-        myset.add(selectRecord.district); 
-    } 
-    
+        option.innerHTML = selectRecord;
+        option.value = selectRecord;
+        temp.append(option);  
     }
 
 }
+function renderRecordsSelect(selectRecords) {
+    let myset = new Set();
+    let myset1 = new Set();
+    let myset2 = new Set();
+    for (selectRecord of selectRecords) {
+        myset.add(selectRecord.admArea);
+        myset1.add(selectRecord.typeObject);
+        myset2.add(selectRecord.district);
+           
+    }
+    AddOption(myset, 'admArea1'); 
+    AddOption(myset, 'admArea');
+    AddOption(myset1, 'typObj1');
+    AddOption(myset1, 'typeObject');
+    AddOption(myset2, 'dist1'); 
+    AddOption(myset2, 'district'); 
+    myset.clear();
+    myset1.clear();
+    myset2.clear();
+    /* 
+    for (selectRecord of myset){
+        option = document.createElement('option');
+        option.innerHTML = selectRecord;
+        option.value = selectRecord;
+        adm.append(option);
+    } */
+
+    
+}
+
 function deleteBtnHandler(event) {
+
     let url = new URL(record_path(event.target.dataset.recordId), host);
-    sendRequest(url,'DELETE', function () {
+    sendRequest(url,'DELETE', function () {                                      //////////////////////////////////////////////////////
         document.getElementById(this.response).remove();
         
 
@@ -43,14 +101,16 @@ function deleteBtnHandler(event) {
 
 
 }
+
 function editBtnHandler(event){
     let url = new URL(record_path(event.target.dataset.recordId), host);
     sendRequest(url, 'GET', function() {
- 
+        editRecord(this.response);
         
     })
     let adm = document.getElementById('staticBackdropLabel');
     adm.innerHTML = "Редактировать запись";
+
 
 }
 function renderRecord(record){
@@ -75,6 +135,9 @@ function renderRecord(record){
     btn.classList.add('btn');
     btn.classList.add('btn-outline-danger');
     btn.classList.add('btn-sm');
+    btn.setAttribute("data-target","#exampleModal");
+    btn.setAttribute("data-toggle","modal");
+
     btn.onclick = deleteBtnHandler;
     td.append(btn);
     edit = document.createElement('i');
@@ -111,7 +174,21 @@ function sendRequest(url, method, onloadHandler, params){
 }
 let host = "http://exam-2020-1-api.std-400.ist.mospolytech.ru";
 let records_path = "/api/data1";
-let myset = new Set();
+
+function Success(msg){
+    let alerttrigger = document.getElementById('for-alerts');
+    let succalert = document.createElement('div');
+    succalert.classList.add('alert');
+    succalert.classList.add('alert-info');
+    succalert.classList.add('my-0');
+    succalert.innerHTML = msg;
+    alerttrigger.append(succalert);
+    setTimeout( () => succalert.remove(), 5000)
+}
+document.getElementById('createNewBtn').onclick = function (){
+
+    document.getElementById('createForm').reset();
+}
 window.onload = function() {
 
     let url = new URL(records_path, host);
@@ -128,15 +205,17 @@ window.onload = function() {
         }, params);
 
     }
-
+  
     document.getElementById('createBtn').onclick = function (){
-        
+
         let url = new URL(records_path, host);
         let params = new FormData(document.getElementById('createForm'));
         sendRequest(url,'POST', function () {
             document.getElementById('records').querySelector('tbody').append(renderRecord(this.response));
 
-        }, params);
-
+        }, params );
+        Success(`Заведение успешно добавлено`);
+        document.getElementById('createForm').reset();
     }
+
 }
